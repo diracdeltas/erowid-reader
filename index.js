75eea36a700f739598e5f9070eb0b3cb982ac098
yan <yan@brave.com>
2020-04-15 22:23:02 -0700

    Revert "autoplay"
    This reverts commit 29c4b6c846e6ca52d04f948324c71bb4d9ee7483.

--- index.js
+++ index.js
@@ -15,31 +15,26 @@ function uniform (array) {
 // put a random text in so that it doesn't fail before the list is loaded
 let list = ['erowid_33369.txt']
 
-const synth = window.speechSynthesis
-
-const say = (text) => {
-  const phrase = new window.SpeechSynthesisUtterance(text)
-  phrase.volume = 1
-  phrase.pitch = randRange(0, 2)
-  synth.speak(phrase)
-}
-const main = () => {
-  window.fetch('/reports/' + uniform(list)).then(async (resp) => {
-    const text = await resp.text()
-    say(text)
-  })
-}
-
 window.fetch('/reports.txt').then(async (resp) => {
   const text = await resp.text()
   list = text.split('\n')
 })
 
 window.onload = () => {
-  main()
+  const synth = window.speechSynthesis
   const start = document.querySelector('#start')
+  const say = (text) => {
+    const phrase = new window.SpeechSynthesisUtterance(text)
+    phrase.volume = 1
+    phrase.pitch = randRange(0, 2)
+    synth.speak(phrase)
+  }
   start.addEventListener('click', (e) => {
+    start.innerText = 'Next'
     synth.cancel()
-    main()
+    window.fetch('/reports/' + uniform(list)).then(async (resp) => {
+      const text = await resp.text()
+      say(text)
+    })
   })
 }
